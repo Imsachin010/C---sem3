99bdfa8f33df059b28f65f7c582a5e2455893379
sachin <007mishrasachinmishra@gmail.com>
2023-01-19 22:38:02 +0530

    solved power of n

---
+++ power of n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{ 
+  int a,base,power;
+  cout<<"enter base No. :";
+  cin>>base;
+  cout<<"enter power :";
+  cin>>power;
+  a = pow(base,power);
+  cout<<a<< endl;
+  }
