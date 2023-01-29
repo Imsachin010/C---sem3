cba96b59f5a4b6c555ae088cb4c6e2515ee5ad32
sachin <007mishrasachinmishra@gmail.com>
2023-01-30 00:17:55 +0530

    Solved area of sphere

---
+++ areaofsphere.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  double r;
+  cout<<"enter radius of sphere :";
+  cin>>r;
+  double area = 4*3.14*r*r;
+  cout<<"Area of sphere is :"<<area<<endl;
+}
